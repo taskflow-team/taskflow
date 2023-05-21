ec3fd652680c93daf24e76fb8e504bea8acf446c
gabriel <gabrielcastro.developer@gmail.com>
2023-05-21 20:20:52 -0300

    translating sign up page

--- app/view/usuario/form.php
+++ app/view/usuario/form.php
@@ -15,47 +15,47 @@
             <img src="../view/img/logo.png" alt="Logo">
         </div>
          <h3 class="text-center">
-             <?php if($dados['id'] == 0) echo "Inserir"; else echo "Alterar"; ?> 
-             Usuário
+             <!-- <?php if($dados['id'] == 0) echo "Inserir"; else echo "Alterar"; ?> 
+             Usuário -->
+             Create your account
         </h3>
         <form id="frmUsuario" method="POST" action="<?= BASEURL ?>/controller/UsuarioController.php?action=save">
             <div class="form-group">
                 <input class="form-control" type="text" id="txtNome" name="nome"
-                    maxlength="70" placeholder="Informe o nome"
+                    maxlength="70" placeholder="Full name"
                     value="<?php echo (isset($dados["usuario"]) ? $dados["usuario"]->getNome() : ''); ?>" />
             </div>
 
             <div class="form-group">
                 <input class="form-control" type="email" id="txtEmail" name="email"
-                    maxlength="15" placeholder="Informe seu email"
+                    maxlength="15" placeholder="Your email"
                     value="<?php echo (isset($dados["usuario"]) ? $dados["usuario"]->getEmail() : ''); ?>"/>
             </div>
 
             <div class="form-group">
                 <input class="form-control" type="text" id="txtLogin" name="login"
-                    maxlength="15" placeholder="Informe o login"
+                    maxlength="15" placeholder="Your login nickname"
                     value="<?php echo (isset($dados["usuario"]) ? $dados["usuario"]->getLogin() : ''); ?>"/>
             </div>
 
             <div class="form-group">
                 <input class="form-control" type="password" id="txtPassword" name="senha"
-                    maxlength="15" placeholder="Informe a senha"
+                    maxlength="15" placeholder="Password"
                     value="<?php echo (isset($dados["usuario"]) ? $dados["usuario"]->getSenha() : ''); ?>"/>
             </div>
 
             <div class="form-group">
                 <input class="form-control" type="password" id="txtConfSenha" name="conf_senha"
-                    maxlength="15" placeholder="Informe a confirmação da senha"
+                    maxlength="15" placeholder="Password confirmation"
                     value="<?php echo isset($dados['confSenha']) ? $dados['confSenha'] : '';?>"/>
             </div>
 
             <input type="hidden" id="hddId" name="id"
                 value="<?= $dados['id']; ?>" />
 
-            <button type="submit" class="btn btn-success">Gravar</button>
-            <button type="reset" class="btn btn-danger">Limpar</button>
+            <button type="submit" class="btn btn-success">Create</button>
             <div>
-                <p>Don´t have an account? <a href="<?= BASEURL . '/controller/LoginController.php?action=login' ?>">Sign up?</a></p>
+                <p>Already have an account? <a href="<?= BASEURL . '/controller/LoginController.php?action=login' ?>">Log in</a></p>
             </div>
         </form>
     </div>
