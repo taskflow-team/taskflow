dcf5818d0a2758483404888b54385e8f7e6fd5b3
gabriel <gabrielcastro.developer@gmail.com>
2023-05-21 21:48:45 -0300

    tasks input

--- app/view/home/index.php
+++ app/view/home/index.php
@@ -5,3 +5,18 @@
 require_once(__DIR__ . "/../include/header.php"); //meta links and botstrap
 require_once(__DIR__ . "/../include/sidebar.php");
 ?>
+
+<link rel="stylesheet" type="text/css" href="<?php echo BASEURL; ?>/view/home/index.css">
+
+<div class="pseudo-body">
+    <form action="">
+        <input type="text" placeholder="Write your task here">
+        <button type="submit" class="btn btn-success">Add task</button>
+    </form>
+
+    <h3>Unfinished Tasks</h3>
+
+    <ul class="tasks">
+
+    </ul>
+</div>
