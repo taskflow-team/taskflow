87c52e8fff5a7208fb521a7f92764ad5f494dc12
gabriel <gabrielcastro.developer@gmail.com>
2023-05-21 21:02:51 -0300

    new icons

--- app/view/include/sidebar.php
+++ app/view/include/sidebar.php
@@ -6,11 +6,23 @@ require_once(__DIR__ . "/../../controller/AcessoController.php");
 <link rel="stylesheet" type="text/css" href="<?php echo BASEURL; ?>/view/include/sidebar.css">
 
 <div class="sidebar">
-    <img src="../view/img/logo.png" alt="Logo">
+    <img class="logo" src="../view/img/logo.png" alt="Logo">
     <nav>
-        <a href="<?= BASEURL . '/controller/HomeController.php?action=home'?>">Home</a>
-        <a href="#">Groups</a>
-        <a href="#">Profile</a>
+        <a href="<?= BASEURL . '/controller/HomeController.php?action=home'?>">
+            <img class="icon" src="<?= BASEURL . '/view/icons/home.png'?>" alt="home icon">
+            Home
+        </a>
+        <a href="#">
+            <img class="icon" src="<?= BASEURL . '/view/icons/groups.png'?>" alt="groups icon">
+            Groups
+        </a>
+        <a href="#">
+            <img class="icon" src="<?= BASEURL . '/view/icons/profile.png'?>" alt="profile icon">
+            Profile
+        </a>
     </nav>
-    <a class="logout-btn" href="<?= LOGOUT_PAGE ?>">Logout</a>
+    <a class="logout-btn" href="<?= LOGOUT_PAGE ?>">
+        <img class="icon" src="<?= BASEURL . '/view/icons/logout.png'?>" alt="logout icon">
+        Logout
+    </a>
 </div>
