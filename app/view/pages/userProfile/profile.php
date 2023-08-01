2ea1f966ead0ddc80cb780f792267ad61b413980
gabriel <gabrielcastro.developer@gmail.com>
2023-07-31 21:45:52 -0300

    dark-input on the profile.css

--- app/view/pages/userProfile/profile.php
+++ app/view/pages/userProfile/profile.php
@@ -13,11 +13,15 @@
             <h2><?php echo $dados["usuario"]->getNome() ?></h2>
             <h2><?php echo $dados["usuario"]->getNivel() ?></h2>
             <p>Finished tasks: <?php echo $dados["usuario"]->getTarefas_Concluidas() ?></p>
-            
+
             <form id="frmEditUsuario" method="POST" action="<?= BASEURL ?>/controller/UsuarioController.php?action=edit">
                 <label for="email">Email:</label>
                 <div>
-                    <input type="email" id="txtEmail" name="email"
+                    <input
+                        class="dark-input"
+                        type="email"
+                        id="txtEmail"
+                        name="email"
                         placeholder="Your email" readonly
                         required
                         value="<?php echo $dados["usuario"]->getEmail(); ?>"
@@ -27,7 +31,11 @@
 
                 <label for="senha">Password:</label>
                 <div>
-                    <input type="password" id="txtPassword" name="senha"
+                    <input
+                        class="dark-input"
+                        type="password"
+                        id="txtPassword"
+                        name="senha"
                         placeholder="Password" readonly
                         required
                         value="<?php echo $dados["usuario"]->getSenha(); ?>"
@@ -37,7 +45,7 @@
                 </div>
 
                 <input type="hidden" name="id" value="<?php echo $dados["usuario"]->getId(); ?>" />
-                
+
                 <?php
                     if (isset($msgSucesso) && trim($msgSucesso) != "") {
                         echo "<div class='alert alert-success'>" . $msgSucesso . "</div>";
@@ -55,7 +63,7 @@
     const btnPassword = document.querySelector('#btnPassword');
     const passwordInput = document.querySelector('#txtPassword');
     const btnShowPassword = document.querySelector('.eyeIcon');
-    
+
     btnPassword.addEventListener("click", (event) => {
         event.preventDefault();
         passwordInput.removeAttribute('readonly');
@@ -64,7 +72,7 @@
     btnEmail.addEventListener('click', (event) => {
         event.preventDefault();
         emailInput.removeAttribute('readonly');
-    }); 
+    });
 
     btnShowPassword.addEventListener('click', (event) => {
         event.preventDefault();
